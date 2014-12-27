58d1f0902560f3321bd77fc0c6f124a88d49bbcb
Alex <alboghardau@gmail.com>
2014-12-27 17:15:04 +0200

    Small changes

--- app/src/main/java/itmc/instanttrivia/Main_Menu.java
+++ app/src/main/java/itmc/instanttrivia/Main_Menu.java
@@ -1,9 +1,14 @@
 package itmc.instanttrivia;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
@@ -14,11 +19,37 @@ public class Main_Menu extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__menu);
 
-        TextView play = (TextView) findViewById(R.id.text_play);
+        final TextView play = (TextView) findViewById(R.id.text_play);
+
+        final Animation anim1;
+
+
+        anim1 = AnimationUtils.loadAnimation(this, R.anim.anim1);
+        final LinearLayout lin = (LinearLayout) findViewById(R.id.main_menu_linear);
+
+        anim1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                lin.removeViewAt(0);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                start_time_game();
+                play.startAnimation(anim1);
+
+
             }
         });
     }
@@ -28,7 +59,6 @@ public class Main_Menu extends ActionBarActivity {
     {
         Intent start = new Intent(this, Game_Timer.class);
         startActivity(start);
-
     }
 
 
