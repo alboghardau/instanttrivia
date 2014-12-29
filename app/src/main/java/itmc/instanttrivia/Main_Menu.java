7586729debde44511b18e92d5401a811526ed57e
Alex <alboghardau@gmail.com>
2014-12-29 20:28:12 +0200

    Small edit on main menu

--- app/src/main/java/itmc/instanttrivia/Main_Menu.java
+++ app/src/main/java/itmc/instanttrivia/Main_Menu.java
@@ -10,8 +10,8 @@ import android.view.View;
 import android.view.ViewManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextSwitcher;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
@@ -20,98 +20,53 @@ import org.w3c.dom.Text;
 
 public class Main_Menu extends ActionBarActivity {
 
-    LinearLayout lin;
-    ViewSwitcher vs;
+    private LinearLayout lin;
+    private ViewSwitcher play_vs;
+    private TextView text_play;
+    private ImageView play_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__menu);
 
+        //variable declaration
+        play_vs = (ViewSwitcher) findViewById(R.id.play_switcher);
         lin = (LinearLayout) findViewById(R.id.main_menu_linear);
+        text_play = (TextView) findViewById(R.id.text_play);
+        play_back = (ImageView) findViewById(R.id.play_back_icon);
 
-        final TextView play = (TextView) findViewById(R.id.text_play);
-        final LinearLayout lin = (LinearLayout) findViewById(R.id.main_menu_linear);
+        switch_play(); // set view switch for play
 
-
-        final Animation anim1;
-        anim1 = AnimationUtils.loadAnimation(this, R.anim.anim1);
-
-
-        //test area
-        Animation in = AnimationUtils.loadAnimation(this,android.R.anim.slide_in_left);
-        Animation out = AnimationUtils.loadAnimation(this,android.R.anim.slide_out_right);
-
-        vs = (ViewSwitcher) findViewById(R.id.viewSwitcher);
-
-        vs.setInAnimation(anim1);
-        vs.setOutAnimation(out);
-
-
-
-        TextView t = (TextView) findViewById(R.id.textView2);
-        t.setOnClickListener(new View.OnClickListener() {
+        text_play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                vs.showPrevious();
+                play_vs.showNext();
             }
         });
-        TextView t2 = (TextView) findViewById(R.id.textView);
-        t2.setOnClickListener(new View.OnClickListener() {
+        play_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                vs.showNext();
+                play_vs.showPrevious();
             }
         });
 
 
-        anim1.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
 
-            }
 
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                removeview(0);
-                createtv(0);createtv(0);
-            }
 
-            @Override
-            public void onAnimationRepeat(Animation animation) {
 
-            }
-        });
 
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                play.startAnimation(anim1);
-            }
-        });
     }
 
-    //remove view from LinearLayout
-    private void removeview(final int id)
-    {
-        new Handler().post(new Runnable() {
-            @Override
-            public void run() {
-                lin.removeViewAt(id);
-            }
-        });
+    //set up switch view play details
+    private void switch_play(){
+        Animation in = AnimationUtils.loadAnimation(this,R.anim.anim2);
+        Animation out = AnimationUtils.loadAnimation(this,R.anim.anim1);
+
+        play_vs.setInAnimation(in);
+        play_vs.setOutAnimation(out);
     }
-
-    private void createtv(int id)
-    {
-        TextView t = new TextView(this);
-        t.setText("Test");
-        t.setTextSize(15);
-
-        lin.addView(t);
-    }
-
-
 
     //start timer game activity
     private void start_time_game()
