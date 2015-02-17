1a17fde670581877a344df8fce34894c860a0d11
Alex <alboghardau@gmail.com>
2015-02-17 21:44:52 +0200

    Reworked settings activity

--- app/src/main/java/com/itmc/instanttrivia/Options.java
+++ app/src/main/java/com/itmc/instanttrivia/Options.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
@@ -16,7 +17,9 @@ public class Options extends ActionBarActivity {
     SharedPreferences settings;
 
     RadioButton radio_red, radio_blue, radio_purple, radio_lgreen, radio_orange;
+    RadioButton opt_radio_easy, opt_radio_medium, opt_radio_hard, opt_radio_random;
     TextView opt_color_header, opt_question_diff;
+    RelativeLayout opt_rel_easy, opt_rel_medium, opt_rel_hard, opt_rel_random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +42,21 @@ public class Options extends ActionBarActivity {
         radio_purple = (RadioButton)findViewById(R.id.radion_purple);
         radio_lgreen = (RadioButton)findViewById(R.id.radio_lgreen);
         radio_orange = (RadioButton)findViewById(R.id.radio_orange);
+
+        opt_radio_easy = (RadioButton)findViewById(R.id.opt_radio_easy);
+        opt_radio_medium = (RadioButton)findViewById(R.id.opt_radio_medium);
+        opt_radio_hard = (RadioButton)findViewById(R.id.opt_radio_hard);
+        opt_radio_random = (RadioButton)findViewById(R.id.opt_radio_random);
+
         opt_color_header = (TextView) findViewById(R.id.opt_color_header);
         opt_question_diff = (TextView) findViewById(R.id.opt_question_diff);
+        opt_rel_easy = (RelativeLayout) findViewById(R.id.opt_rel_easy);
+        opt_rel_medium = (RelativeLayout) findViewById(R.id.opt_rel_medium);
+        opt_rel_hard = (RelativeLayout) findViewById(R.id.opt_rel_hard);
+        opt_rel_random = (RelativeLayout) findViewById(R.id.opt_rel_random);
+
         radio_check(settings.getString("Color_Theme", "Purple"));
+        diff_check(settings.getInt("question_diff", 5));
 
         //sets rest of theme settings
         Theme_Setter2();
@@ -74,6 +89,24 @@ public class Options extends ActionBarActivity {
                 break;
             case "Orange":
                 radio_orange.setChecked(true);
+                break;
+        }
+    }
+
+    //set on start the difficulty
+    private void diff_check(int diff){
+        switch (diff){
+            case 1:
+                opt_radio_easy.setChecked(true);
+                break;
+            case 2:
+                opt_radio_medium.setChecked(true);
+                break;
+            case 3:
+                opt_radio_hard.setChecked(true);
+                break;
+            case 5:
+                opt_radio_random.setChecked(true);
                 break;
         }
     }
@@ -127,8 +160,8 @@ public class Options extends ActionBarActivity {
         }
     }
 
-
-    private void update_options(String option){
+    //sets the color setting in preferences and restarts app
+    private void update_options_color(String option){
         SharedPreferences.Editor editor = settings.edit();
         editor.putString("Color_Theme", option);
         editor.commit();
@@ -139,56 +172,90 @@ public class Options extends ActionBarActivity {
         startActivity(i);
     }
 
+    //sets the question difficulty setting in preferences
+    private void update_options_difficulty(int difficulty){
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("question_diff", difficulty);
+        editor.commit();
+    }
+
+    //question difficulty radio clicker
+    public void onDifficultyClicked(View view){
+        switch (view.getId()){
+            case R.id.opt_rel_easy:
+                update_options_difficulty(1);
+                radio_diff_reset();
+                opt_radio_easy.setChecked(true);
+                break;
+            case R.id.opt_rel_medium:
+                update_options_difficulty(2);
+                radio_diff_reset();
+                opt_radio_medium.setChecked(true);
+                break;
+            case R.id.opt_rel_hard:
+                update_options_difficulty(3);
+                radio_diff_reset();
+                opt_radio_hard.setChecked(true);
+                break;
+            case R.id.opt_rel_random:
+                update_options_difficulty(5);
+                radio_diff_reset();
+                opt_radio_random.setChecked(true);
+                break;
+        }
+    }
+
+    private void radio_diff_reset(){
+        opt_radio_easy.setChecked(false);
+        opt_radio_medium.setChecked(false);
+        opt_radio_hard.setChecked(false);
+        opt_radio_random.setChecked(false);
+    }
+
+    //resets radios for color selection
     public void onRadioButtonClicked(View view){
 
         switch (view.getId()){
 
             //RED BUTTON
             case R.id.linear_opt_red:
-                update_options("Red");
+                update_options_color("Red");
+                radio_color_reset();
                 radio_red.setChecked(true);
-                radio_blue.setChecked(false);
-                radio_lgreen.setChecked(false);
-                radio_purple.setChecked(false);
-                radio_orange.setChecked(false);
                 break;
 
             //BLUE BUTTOn
             case R.id.linear_opt_blue:
-                update_options("Blue");
-                radio_red.setChecked(false);
+                update_options_color("Blue");
+                radio_color_reset();
                 radio_blue.setChecked(true);
-                radio_lgreen.setChecked(false);
-                radio_purple.setChecked(false);
-                radio_orange.setChecked(false);
                 break;
 
             //LGREEN BUTTON
             case R.id.linear_opt_lgreen:
-                update_options("LGreen");
-                radio_red.setChecked(false);
-                radio_blue.setChecked(false);
+                update_options_color("LGreen");
+                radio_color_reset();
                 radio_lgreen.setChecked(true);
-                radio_purple.setChecked(false);
-                radio_orange.setChecked(false);
                 break;
 
             //PURPLE BUTTON
             case R.id.linear_opt_purple:
-                update_options("Purple");
-                radio_red.setChecked(false);
-                radio_blue.setChecked(false);
-                radio_lgreen.setChecked(false);
+                update_options_color("Purple");
+                radio_color_reset();
                 radio_purple.setChecked(true);
-                radio_orange.setChecked(false);
                 break;
             case R.id.linear_opt_orange:
-                update_options("Orange");
-                radio_red.setChecked(false);
-                radio_blue.setChecked(false);
-                radio_lgreen.setChecked(false);
-                radio_purple.setChecked(false);
+                update_options_color("Orange");
+                radio_color_reset();
                 radio_orange.setChecked(true);
         }
+    }
+
+    private void radio_color_reset(){
+        radio_red.setChecked(false);
+        radio_blue.setChecked(false);
+        radio_lgreen.setChecked(false);
+        radio_purple.setChecked(false);
+        radio_orange.setChecked(false);
     }
 }
