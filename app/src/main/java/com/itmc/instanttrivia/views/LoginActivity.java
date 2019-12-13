0cd51606ae5b0adc6a03c0b3179543912300cd99
albog <alboghardau@gmail.com>
2019-12-13 13:39:39 +0200

    sync

--- app/src/main/java/com/itmc/instanttrivia/views/LoginActivity.java
+++ app/src/main/java/com/itmc/instanttrivia/views/LoginActivity.java
@@ -3,6 +3,8 @@ package com.itmc.instanttrivia.views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.itmc.instanttrivia.R;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
